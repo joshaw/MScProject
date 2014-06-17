f4cae0bf4fea239bf9d8747faf04945ca8ff1937
Josh Wainwright <jaw097@bham.ac.uk>
2014-06-17 11:58:13 +0100

    Start new implementation which allows adding arbitrary points to a quadtree.

---
+++ Code/QuadTreeData/QuadTree.java
@@ -0,0 +1,72 @@
+import java.util.ArrayList;
+
+public class QuadTree {
+
+	private boolean root = false;
+	private int maxX;
+	private int maxY;
+
+	private QuadTree tl = new QuadTree();
+	private QuadTree tr = new QuadTree();
+	private QuadTree bl = new QuadTree();
+	private QuadTree br = new QuadTree();
+	private ArrayList<Coordinate> points;
+	private boolean leaf;
+
+	public QuadTree(int maxX, int maxY){
+		this.root = true;
+		this.maxX = maxX;
+		this.maxY = maxY;
+	}
+
+	/** Creates a leaf node of a quadtree.
+	 */
+	public QuadTree() {
+		this.leaf = true;
+	}
+
+	public boolean isLeaf() {
+		return leaf;
+	}
+
+	public ArrayList<Coordinate> getPoints() {
+		return points;
+	}
+
+	public void addPoint(Coordinate c) {
+		this.points.add(c);
+	}
+
+	public QuadTree getTL() {
+		return tl;
+	}
+
+	public void setTL(QuadTree t) {
+		this.tl = t;
+	}
+
+	public QuadTree getTR() {
+		return tr;
+	}
+
+	public void setTR(QuadTree t) {
+		this.tr = t;
+	}
+
+	public QuadTree getBL() {
+		return bl;
+	}
+
+	public void setBL(QuadTree t) {
+		this.bl = t;
+	}
+
+	public QuadTree getBR() {
+		return br;
+	}
+
+	public void setBR(QuadTree t) {
+		this.br = t;
+	}
+
+}
