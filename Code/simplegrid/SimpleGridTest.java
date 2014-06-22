b2e5a316d681b1867bb0203c3cc78ce4c7f9074d
Josh Wainwright <wainwright.ja@gmail.com>
2014-06-22 10:31:16 +0000

    Use commandline arguments for settings

--- Code/simplegrid/SimpleGridTest.java
+++ Code/simplegrid/SimpleGridTest.java
@@ -7,10 +7,24 @@ public class SimpleGridTest {
 
 	public static void main(String[] args) {
 
-		SimpleGrid s = new SimpleGrid(41000, 41000, 10, "sampledata/palm_1.txt");
+		SimpleGrid main;
+		if (args.length == 4) {
+			double xdim = Double.parseDouble(args[0]);
+			double ydim = Double.parseDouble(args[1]);
+			int density = Integer.parseInt(args[2]);
+			main = new SimpleGrid(xdim, ydim, density, args[3]);
+		} else if (args.length == 3) {
+			int density = Integer.parseInt(args[0]);
+			double scale = Double.parseDouble(args[1]);
+			main = new SimpleGrid(41000.0, 41000.0, density, args[2]);
+		} else if (args.length == 1) {
+			main = new SimpleGrid(41000.0, 41000.0, 200, args[0]);
+		} else {
+			main = new SimpleGrid(41000.0, 41000.0, 200, "sampledata/palm_1.txt");
+		}
 
 		// System.out.println(s.toString());
-		s.writeToFile("analysis.pnm");
+		main.writeToFile("analysis.pnm");
 
 	}
 }
