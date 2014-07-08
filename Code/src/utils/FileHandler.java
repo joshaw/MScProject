6f375779b98c29b4d51519dcc91d9675010bce9f
Josh Wainwright <jaw097@bham.ac.uk>
2014-07-08 12:46:51 +0100

    Create FileHandler class for doing general things with files

---
+++ Code/src/utils/FileHandler.java
@@ -0,0 +1,57 @@
+// Created:  Tue 08 Jul 2014 12:41 PM
+// Modified: Tue 08 Jul 2014 12:43 PM
+
+package utils;
+
+import utils.Coordinate;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileHandler {
+
+	public static Coordinate getMaxCoord(String filepath, int colX, int colY, String separator) {
+		Coordinate maxCoord = new Coordinate(0,0);
+		if (!filepath.equals("")) {
+			BufferedReader reader = null;
+
+			try {
+				reader = new BufferedReader(new FileReader(filepath));
+				String line = null;
+
+				reader.readLine();
+				while ((line = reader.readLine()) != null) {
+
+					String[] entries = line.split(separator);
+					double[] xyDouble = new double[2];
+					xyDouble[0] = Double.parseDouble(entries[colX]);
+					xyDouble[1] = Double.parseDouble(entries[colY]);
+
+					if (xyDouble[0] > maxCoord.getX()) {
+						maxCoord.setX(xyDouble[0]);
+						System.out.println("Max x: " + xyDouble[0]);
+					}
+					if (xyDouble[1] > maxCoord.getY()) {
+						maxCoord.setY(xyDouble[1]);
+						System.out.println("Max y: " + xyDouble[1]);
+					}
+
+					// countFile++;
+				}
+			} catch (IOException e) {
+				System.err.println("Error: Could not open file " + filepath);
+			} finally {
+				if (reader != null) {
+					try {
+						reader.close();
+					} catch(IOException e){
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		return maxCoord;
+	}
+
+}
